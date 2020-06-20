da21aca29b75b86bcd51c69f0a59300eb19f7594
akashbangar <akash.bangar426@gmail.com>
2020-06-20 22:11:10 +0530

    Add files via upload
    probability of getting HEAD by tossing a fair coin

---
+++ coin_toss.m
@@ -0,0 +1,20 @@
+num_experiments =  round(rand()*1000)
+num_flips = round(rand()*10000)
+
+experiment = zeros(1,num_experiments);
+prob_heads = zeros(1,num_experiments);
+
+for i=1:num_experiments
+  toss = round(rand(1,num_flips));
+  num_heads = length(find(toss));
+  prob_heads(i) = num_heads/num_flips;
+  experiment(i)=i;
+endfor
+
+disp(mean(prob_heads));
+plot(experiment,prob_heads,'o');
+xlabel("experiments");
+ylabel("probability of head");
+title("coin toss");
+grid on
+print -djpg coin_toss
